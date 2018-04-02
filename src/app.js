4011036f835e38836f930a257cfbc468379f1d16
Dave Fooks <davidfooks@turbulenz.biz>
2018-04-02 11:31:13 +0100

    WebGL rendering with orthographic projection

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ var App = function ()
     this.debugText = "";
 
     this.canvas = document.getElementById("canvas");
-    this.ctx = this.canvas.getContext("2d");
+    //this.ctx = this.canvas.getContext("2d");
+    this.gl = this.canvas.getContext("webgl");
+
+    if (!this.gl) {
+        alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+        return;
+    }
+
+    this.initWebGL();
 
     this.stage = new Stage(this.canvas);
     this.stage.enable();
@@ -98,12 +106,84 @@ App.prototype.update = function update()
         this.nextTick = 30;
     }
 
-    this.breadboard.update();
+    //this.breadboard.update();
     this.save();
 };
 
 App.prototype.updateGame = function updateGame()
 {
+
+};
+
+App.prototype.initWebGL = function initWebGL()
+{
+    // this.scene = new THREE.Scene();
+
+    // this.geometry = new THREE.BoxBufferGeometry( 0.75, 0.75, 0.75 );
+
+    // var mesh = new THREE.Mesh(this.geometry, this.material);
+
+    // this.scene.add(mesh);
+
+    // this.renderer = new THREE.WebGLRenderer({canvas: this.canvas});
+    // this.renderer.setPixelRatio( window.devicePixelRatio );
+
+    var  renderer = new THREE.WebGLRenderer({canvas: this.canvas});
+
+    // There's no reason to set the aspect here because we're going
+    // to set it every frame any
+
+    var aspect = this.canvas.width / this.canvas.height;
+    var frustumSize = 600;
+
+    var  camera = new THREE.OrthographicCamera(-1, 1, -1, 1, 0, 5);
+    camera.position.z = 2;
+
+    var scene = new THREE.Scene();
+    var geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
+
+    var vertexShaderStr = `
+            varying vec2 vUv;
+            void main()
+            {
+                vUv = uv;
+                vec4 mvPosition = modelViewMatrix * vec4(position, 1.0 );
+                gl_Position = projectionMatrix * mvPosition;
+            }
+    `;
+
+    var fragmentShaderStr = `
+            varying vec2 vUv;
+            void main( void ) {
+                gl_FragColor = vec4( 0.5 - vUv.y * 0.5, vUv.x, vUv.y, 1.0 );
+            }
+    `;
+
+    var material = new THREE.ShaderMaterial({
+        uniforms: {},
+        vertexShader: vertexShaderStr,
+        fragmentShader: fragmentShaderStr
+    });
+
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    var light1 = new THREE.PointLight(0xff80C0, 2, 0);
+    light1.position.set(200, 100, 300);
+    scene.add(light1);
+
+    function animate(time)
+    {
+        time *= 0.001;  // seconds
+
+        mesh.rotation.x = time * 0.5;
+        mesh.rotation.y = time * 1;
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    }
+
+    requestAnimationFrame(animate);
 };
 
 App.prototype.debugInfo = function debugInfo(str)
